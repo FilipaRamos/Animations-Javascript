a2d572a77e3b7c7798888787e8a71123dbfb5dec
IneSantos <inesalexcarneiro@hotmail.com>
2015-11-10 17:20:32 +0000

    animation + Linear Animation

--- reader/LinearAnimation.js
+++ reader/LinearAnimation.js
@@ -1,11 +1,11 @@
-function LinearAnimation(scene, animation, crtlPoints, time, transMatrix){
+function LinearAnimation(scene, animation, crtlPoints, time){
 
  	CGFappearance.call(this,scene);
 
     this.time = time; //total time in sec
     this.animation = animation;
-    this.crtlPoints = crtlPoints;
-    this.transMatrix = transMatrix;
+    this.crtlPoints = crtlPoints; //relative to animation's position 
+   
 }
 
 LinearAnimation.prototype = Object.create(CGFappearance.prototype);
@@ -14,6 +14,18 @@ LinearAnimation.prototype.constructor = LinearAnimation;
 
 LinearAnimation.prototype.execute = function()
 {
+    var vecCtrlPA = vec3.create();
+    var vecCtrlPB = vec3.create();
+    var vecDistancia = vec3.create();
+
+    for(ver i = 0; i < this.crtlPoints.length; i++){
+        
+          vecCtrlPA = this.crtlPoints[i];
+          vecCtrlPB = this.crtlPoints[i+1];
+          vecDistancia = vec3.distance(vecCtrlPA, vecCtrlPB);
+          
+    }
+
 
    
     
