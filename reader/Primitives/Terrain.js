a1c5a1868b78e0416b59c6acafe177a30daa7dbd
IneSantos <inesalexcarneiro@hotmail.com>
2015-11-14 01:01:21 +0000

    Terrain  created

---
+++ reader/Primitives/Terrain.js
@@ -0,0 +1,11 @@
+function Terrain(scene, divisions){
+
+ 	CGFobject.call(this,scene);
+
+    	
+
+
+};
+
+Terrain.prototype = Object.create(CGFobject.prototype);
+Terrain.prototype.constructor = Terrain;
